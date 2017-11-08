67e7dffb34939b724bb885414b8f04d11475be35
Dustin Mundy <Dustin.A.Mundy@gmail.com>
2017-11-08 12:03:43 -0800

    bugfix

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 'use strict';
 
 const express = require('express')
+const pg = require('pg');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const CLIENT_URL = process.env.CLIENT_URL;
+const SQL_URL = process.env.DATABASE_URL;
+// postgres://fmpthvocildipl:f4bd1be037fb83b8d4440774a73f6a2f1f54c71dc45e5ae41c2242e83f4ce4bb@ec2-54-163-255-181.compute-1.amazonaws.com:5432/df5aldphdr9fei
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
 
